f2d7e09d117b39222bd66b27ff13e19ba68e4eec
Xelyz <wildon.wu.you@icloud.com>
2024-08-06 15:53:32 +0800

    updates and helps

--- cogs/play.py
+++ cogs/play.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import mysql.connector
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,20 +19,20 @@ class Play(commands.Cog):
         self.bot = bot
         self.games = {}
 
-    @commands.command()
+    @commands.command(help='Play a mini game!')
     async def play(self, ctx, game_name: ToLowerCase):
         channel_id = ctx.channel.id
         if channel_id not in self.games:
             match game_name:
                 case 'hangman':
-                    # self.games[channel_id] = game = Hangman()
-                    await Hangman().startGameMsg(ctx)
+                    self.games[channel_id] = game = Hangman()
+                    await game.startGame(ctx)
                 case _:
-                    await ctx.send('Game Not Found.')
+                    await ctx.send('Game Not Found.\nAvailable Games: Hangman')
         else:
             await ctx.send("A game is already running in this channel!")
 
-    @commands.command()
+    @commands.command(help='Ends the game in the channel')
     async def endgame(self, ctx):
         channel_id = ctx.channel.id
         if channel_id in self.games:
@@ -40,17 +41,66 @@ class Play(commands.Cog):
         else:
             await ctx.send('There are no games running in this channel...')
 
-    @commands.command()
-    async def rand(self, ctx):
-        cursor.execute('SELECT * FROM songs ORDER BY RAND() LIMIT 1')
-        await ctx.send(cursor.fetchone())
+    @commands.command(help='Hangman game command. View a character')
+    async def view(self, ctx, s):
+        channel_id = ctx.channel.id
+        if channel_id in self.games:
+            game = self.games[channel_id]
+            msg = game.view(s, ctx.author)
+            await game.currState(ctx, msg)
+        else:
+            await ctx.send('There are no games running. Perhaps you want to start a game by sending \"lowiro-play (game)?\"')
 
 class Hangman():
     def __init__(self):
         self.name = 'hangman'
+        self.answer = []
+        self.state = []
+        self.songs = []
+        self.viewed = []
 
-    async def startGameMsg(self, ctx):
-        await ctx.send('game still under development')
+    async def startGame(self, ctx):
+        n = 6
+        cursor.execute("""
+        SELECT s.song_id, MAX(lt.title) AS title
+        FROM songs s
+        JOIN localized_titles lt ON s.song_id = lt.song_id
+        WHERE lt.title REGEXP '^[A-Za-z ]+$'
+        GROUP BY s.song_id
+        HAVING COUNT(lt.title) = 1
+        ORDER BY RAND()
+        LIMIT %s;
+        """,(n,))
+        result = cursor.fetchall()
+        for song_id, title in result:
+            self.songs.append(song_id)
+            self.answer.append(title)
+            self.state.append(re.sub('[a-zA-Z]', '\\*', title))
+        await self.currState(ctx, 'Game Start!\n')
+
+    async def currState(self, ctx, note='Current Game:\n'):
+        msg = [note]
+        msg.append(f'Viewed: {','.join(self.viewed)}')
+        for i, song in enumerate(self.state, 1):
+            msg.append(f'{i}: {song}')
+        # for testing purpose
+        for i, song in enumerate(self.answer, 1):
+            msg.append(f'{i}: {song}')
+
+        await ctx.send('\n'.join(msg))
+
+    def view(self, s, user):
+        if not len(s) == 1:
+            return 'The Argument MUST Be A Single Character'
+        if s in self.viewed:
+            return 'Character Already Viewed'
+        self.viewed.append(s)
+        for i, song in enumerate(self.answer):
+            for j, char in enumerate(song):
+                if char == s:
+                    self.state[i][j] = s
+
+        return f'{user.display_name} viewed {s}'
 
 async def setup(bot):
     await bot.add_cog(Play(bot))
