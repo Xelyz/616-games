d1a957775c603d8b18cb3760ca379f766665a145
Xelyz <wildon.wu.you@icloud.com>
2024-08-06 12:11:41 +0800

    some update

---
+++ cogs/info.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def info(self, ctx):
+        await ctx.send('''
+"A harmony of Light awaits you in a lost world of musical Conflict."
+
+In a world of white, and surrounded by “memory”, two girls awaken under glass-filled skies.
+
+Arcaea is a mobile rhythm game for both experienced and new rhythm game players alike, blending novel gameplay, immersive sound, and a powerful story of wonder and heartache. Experience gameplay that reflects the story's emotions and events—and progress to unlock more of this unfurling narrative.
+Challenging trials can be discovered through play, higher difficulties can be unlocked, and a real-time online mode is available to face off against other players.
+''')
+
+async def setup(bot):
+    await bot.add_cog(Info(bot))
--- cogs/play.py
+++ cogs/play.py
@@ -1,7 +1,14 @@
 import discord
 from discord.ext import commands
+import os
+import mysql.connector
+from dotenv import load_dotenv
 
-from ..hangman import Hangman
+load_dotenv()
+PWD = os.getenv('PASSWORD')
+# Establish connection
+conn = mysql.connector.connect(user='discordbot', password=PWD, host='localhost', database='arcaeaSongInfo')
+cursor = conn.cursor()
 
 def ToLowerCase(arg):
     return arg.lower()
@@ -9,17 +16,41 @@ def ToLowerCase(arg):
 class Play(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.games = {}
 
     @commands.command()
-    async def play(self, ctx, game: ToLowerCase):
-        match game:
-            case 'hangman':
-                #implement logic when complete the game
-                game = Hangman()
-                ctx.send(game.startGameMsg())
-            case _:
-                ctx.send('Game Not Found.')
+    async def play(self, ctx, game_name: ToLowerCase):
+        channel_id = ctx.channel.id
+        if channel_id not in self.games:
+            match game_name:
+                case 'hangman':
+                    # self.games[channel_id] = game = Hangman()
+                    await Hangman().startGameMsg(ctx)
+                case _:
+                    await ctx.send('Game Not Found.')
+        else:
+            await ctx.send("A game is already running in this channel!")
 
+    @commands.command()
+    async def endgame(self, ctx):
+        channel_id = ctx.channel.id
+        if channel_id in self.games:
+            await ctx.send(f'{self.games[channel_id].name} in this channel ended...')
+            del self.games[channel_id]
+        else:
+            await ctx.send('There are no games running in this channel...')
+
+    @commands.command()
+    async def rand(self, ctx):
+        cursor.execute('SELECT * FROM songs ORDER BY RAND() LIMIT 1')
+        await ctx.send(cursor.fetchone())
+
+class Hangman():
+    def __init__(self):
+        self.name = 'hangman'
+
+    async def startGameMsg(self, ctx):
+        await ctx.send('game still under development')
 
 async def setup(bot):
     await bot.add_cog(Play(bot))
