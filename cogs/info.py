a67209c31ff6bf3985ee3028ddba2c5a4450b5cd
Xelyz <wilson.wu.you@icloud.com>
2024-08-11 12:22:43 +0800

    Separated SQL query

--- cogs/info.py
+++ cogs/info.py
@@ -6,8 +6,9 @@ class Info(commands.Cog):
         self.bot = bot
 
     @commands.command(help='arcaea game info')
-    async def info(self, ctx):
-        await ctx.send('''
+    async def info(self, ctx, song=None):
+        if song == None:
+            await ctx.send('''
 "A harmony of Light awaits you in a lost world of musical Conflict."
 
 In a world of white, and surrounded by “memory”, two girls awaken under glass-filled skies.
@@ -15,6 +16,8 @@ In a world of white, and surrounded by “memory”, two girls awaken under glas
 Arcaea is a mobile rhythm game for both experienced and new rhythm game players alike, blending novel gameplay, immersive sound, and a powerful story of wonder and heartache. Experience gameplay that reflects the story's emotions and events—and progress to unlock more of this unfurling narrative.
 Challenging trials can be discovered through play, higher difficulties can be unlocked, and a real-time online mode is available to face off against other players.
 ''')
+        else:
+            pass
 
 async def setup(bot):
     await bot.add_cog(Info(bot))
