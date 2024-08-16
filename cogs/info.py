5ae84c1c5c7fbc377b0b4e604c76e35be7de0384
Xelyz <wilson.wu.you@icloud.com>
2024-08-16 12:10:32 +0800

    song info search and add alias

--- cogs/info.py
+++ cogs/info.py
@@ -1,11 +1,34 @@
+from typing import Coroutine
 import discord
 from discord.ext import commands
+from discord.ui import View
+from discord import ButtonStyle
 from .utils.query import Query
 
 cursor = Query()
 
 def ToLowerCase(arg):
     return arg.lower()
+
+def insert(song_id, alias):
+    cursor.execute('''
+INSERT INTO alias (song_id, title)
+VALUES (%s, %s)
+''', (song_id, alias))
+
+class ConfirmView(View):
+    def __init__(self, msg, action, *params):
+        self.msg = msg
+        self.action = action
+        self.params = params
+        super().__init__(timeout=5)
+
+    async def on_timeout(self):
+        await self.msg.edit(view=None)
+
+    @discord.ui.button(label="Confirm", style=ButtonStyle.primary, custom_id="confirm")
+    async def confirm(self, interaction, button):
+        await self.action(*self.params)
 
 class Info(commands.Cog):
     def __init__(self, bot):
@@ -17,6 +40,35 @@ class Info(commands.Cog):
             return
         await ctx.send(err)
         raise err
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, err):
+        if ctx.cog is None or ctx.cog.qualified_name != self.__class__.__name__:
+            return
+        await ctx.send(err)
+        raise err
+    
+    @commands.command(name='add-alias', help='lowiro-add-alias "<song>" "<alias>" add alias to a song for search.')
+    async def add_alias(self, ctx, song: ToLowerCase, alias):
+        result = cursor.execute("""
+            SELECT t.song_id, t.title, 
+            (MATCH(t.title) AGAINST(%s) + 
+            IF(LOWER(t.title) = %s, 100, 0)) AS relevance
+            FROM localized_titles t
+            WHERE MATCH(t.title) AGAINST(%s)
+            ORDER BY relevance DESC;
+            """,(song, song, song))
+        
+        if result:
+            result = result[0]
+            song_id = result[0]
+            title = result[1]
+
+            view = ConfirmView(ctx.message, insert, song_id, alias)
+            msg = await ctx.send(f"Adding alias **{alias}** to song **{title}**.\n*Timeout in 5 seconds*", view=view)
+            view.msg = msg
+        else:
+            await ctx.send("Cannot find the song")
 
     @commands.command(help='arcaea game info')
     async def info(self, ctx, *, song:ToLowerCase = None):
@@ -53,21 +105,21 @@ Challenging trials can be discovered through play, higher difficulties can be un
                 """,(song_id,))[0]
 
                 difficulties = cursor.execute("""
-                SELECT rating_class, rating, refer_to
+                SELECT rating_class, rating, chart_constant, refer_to
                 FROM difficulties 
                 WHERE song_id = %s;
                 """,(song_id,))
 
                 display = []
-                for rating_class, rating, refer_to in difficulties:
+                for rating_class, rating, chart_constant, refer_to in difficulties:
                     if refer_to:
                         res = cursor.execute("""
-                        SELECT rating_class, rating
+                        SELECT rating_class, rating, chart_constant
                         FROM difficulties
                         WHERE song_id = %s AND rating_class = %s;
                         """,(refer_to, rating_class))
                         if res:
-                            rating_class, rating = res[0]
+                            rating_class, rating, chart_constant = res[0]
 
                     match rating_class:
                         case 0:
@@ -80,8 +132,11 @@ Challenging trials can be discovered through play, higher difficulties can be un
                             cls = 'BYD'
                         case 4:
                             cls = 'ETR'
+                    
+                    plus = '+' if chart_constant - int(chart_constant) >= 0.7 else ''
+                    const = f' ({chart_constant})' if chart_constant >= 9.0 else ''
 
-                    display.append(f'[{cls}](https://www.youtube.com/results?search_query=arcaea+{id}+{cls}): {rating}')
+                    display.append(f'[{cls}](https://www.youtube.com/results?search_query=arcaea+{id}+{cls}): {rating}{plus}{const}')
 
                 display = ' / '.join(display)
 
